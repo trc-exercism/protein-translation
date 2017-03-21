fce3602fc3ab55075d8e0783ad8ed87c329ad53f
Devin Nathan-Turner <devin.nathan-turner@thinkresearch.com>
2017-03-21 13:13:00 -0400

    Some just straight up magic

--- protein_translation.exs
+++ protein_translation.exs
@@ -1,34 +1,26 @@
 defmodule ProteinTranslation do
-  @doc """
-  Given an RNA string, return a list of proteins specified by codons, in order.
-  """
   @spec of_rna(String.t()) :: { atom,  list(String.t()) }
   def of_rna(rna) do
+    _of_rna(Enum.map(Enum.chunk(to_charlist(rna), 3), fn (c) -> of_codon(to_string(c)) end), [])
   end
 
-  @doc """
-  Given a codon, return the corresponding protein
+  defp _of_rna([{:error, _}|_], _), do: {:error, "invalid RNA"}
+  defp _of_rna([{:ok, "STOP"}|_], acc), do: {:ok, Enum.uniq(acc)}
+  defp _of_rna([], acc), do: {:ok, Enum.uniq(acc)}
+  defp _of_rna([{:ok, codon}|t], acc), do: _of_rna(t, acc ++ [codon])
 
-  UGU -> Cysteine
-  UGC -> Cysteine
-  UUA -> Leucine
-  UUG -> Leucine
-  AUG -> Methionine
-  UUU -> Phenylalanine
-  UUC -> Phenylalanine
-  UCU -> Serine
-  UCC -> Serine
-  UCA -> Serine
-  UCG -> Serine
-  UGG -> Tryptophan
-  UAU -> Tyrosine
-  UAC -> Tyrosine
-  UAA -> STOP
-  UAG -> STOP
-  UGA -> STOP
-  """
   @spec of_codon(String.t()) :: { atom, String.t() }
   def of_codon(codon) do
+    cond do
+      codon == "UGU" or codon == "UGC" -> { :ok, "Cysteine" }
+      codon == "UUA" or codon == "UUG" -> { :ok, "Leucine" }
+      codon == "AUG" -> { :ok, "Methionine" }
+      codon == "UUU" or codon == "UUC" -> { :ok, "Phenylalanine" }
+      codon == "UCU" or codon == "UCC" or codon == "UCA" or codon == "UCG" -> { :ok, "Serine" }
+      codon == "UGG" -> { :ok, "Tryptophan" }
+      codon == "UAU" or codon == "UAC" -> { :ok, "Tyrosine" }
+      codon == "UAA" or codon == "UAG" or codon == "UGA" -> { :ok, "STOP" }
+      true -> { :error, "invalid codon" }
+    end
   end
 end
-
